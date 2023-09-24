edd73037f4779c56572f87dffbfe48cbd614e2bd
Bryan1625 <bryangomez1625@gmail.com>
2023-09-24 15:39:13 -0500

    comparador de libros

---
+++ laboratorioColecciones_BryanGomez/src/main/java/ComparadorLibros.java
@@ -0,0 +1,18 @@
+import java.util.Comparator;
+
+public class ComparadorLibros {
+
+    public static Comparator<Libro> porTitulo = new Comparator<Libro>() {
+        @Override
+        public int compare(Libro a, Libro b) {
+            return a.getTitulo().compareTo(b.getTitulo());
+        }
+    };
+
+    public static Comparator<Libro> porAutor = new Comparator<Libro>() {
+        @Override
+        public int compare(Libro a, Libro b) {
+            return a.getAutor().compareTo(b.getAutor());
+        }
+    };
+}
